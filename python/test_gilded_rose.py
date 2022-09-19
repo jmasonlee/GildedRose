4aa12f7afeb483f4bbb5adb9671180bfc28574b4
jmasonlee <jmasonlee@gmail.com>
2022-09-19 08:12:11 -0600

    First passing test

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from approvaltests import SimpleLogger, verify
+
 from gilded_rose import LoggingItem, LoggingGildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
+        output = SimpleLogger.log_to_string()
         items = [LoggingItem("foo", 0, 0)]
         gilded_rose = LoggingGildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        verify(output)
 
 
 if __name__ == '__main__':
